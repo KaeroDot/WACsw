ffc371af333d4d7e8059ad91812c53961b5403e6
KaeroDot <KaeroDot@contact.me.on.git.hub>
2025-02-13 14:29:54 +0100

    fix: FR selftest

--- data_processing/FR/selftest.m
+++ data_processing/FR/selftest.m
@@ -1,3 +1,5 @@
+clear all; close all
+
 addpath('acdc')
 addpath('info')
 
@@ -9,14 +11,14 @@ addpath('info')
 			% acdc = correction_load_acdctransfer(file, meas);
 			% tbl = correction_interp_table(acdc.acdc_diff,[2 3 4 5 6],15);
 
-verbose = 0;
+verbose = 1;
 % generate simulated measurement data:
 [M_FR, simulated_digitizer_FR] = G_FR(verbose);
 % process measurement:
 [f, measured_digitizer_FR, ac_source_stability] = P_FR(M_FR, verbose);
 
 figure
-plot(M_FR.f.v, simulated_digitizer_FR.v - 1, '.b', f.v, simulated_digitizer_FR.v - 1, '-r')
+plot(M_FR.f.v, simulated_digitizer_FR.v - 1, '.b', f.v, measured_digitizer_FR.v - 1, '-r')
 title(sprintf('selftest.m\nsimulated and measured frequency response of the digitizer'))
 legend('simulated FR', 'measured FR')
 xlabel('signal frequency (Hz)')

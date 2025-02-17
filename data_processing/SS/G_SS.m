0ae07d41a69b5cd8b4e48ab971d2bc8e24d19256
KaeroDot <KaeroDot@contact.me.on.git.hub>
2025-02-17 14:33:26 +0100

    fix: G_SS bad structures

--- data_processing/SS/G_SS.m
+++ data_processing/SS/G_SS.m
@@ -52,9 +52,9 @@ function [M_SS] = G_SS(varargin);
         M_SS.f_envelope.v = 1;
         M_SS.A_envelope.v = M_SS.A.v;
         M_SS.ph_envelope.v = 0*pi;
-        M_SS.fstep.v = 40*M_SS.f_envelope.v;
+        M_SS.f_step.v = 40*M_SS.f_envelope.v;
         M_SS.L.v = 1e3;
-        M_SS.phstep.v = 0.0314;
+        M_SS.ph_step.v = 0.0314;
         M_SS.fm.v = 1e14;
         M_SS.waveformtype.v = 2;
         M_SS.Rs.v = 0;
@@ -77,13 +77,13 @@ function [M_SS] = G_SS(varargin);
         M_SS.ph_envelope.v = 0;
         % PJVS step change frequency (Hz):
         % (number of PJVS steps in a single envelope (Hz) is given by ratio
-        % fstep/f_envelope)
-        M_SS.fstep.v = 40.*M_SS.f_envelope.v;
+        % f_step/f_envelope)
+        M_SS.f_step.v = 40.*M_SS.f_envelope.v;
         % Length of the record (samples):
         % one period of the PJVS signal
         M_SS.L.v = fix(1.*M_SS.fs.v./M_SS.f_envelope.v);
-        % phstep - phase of the PJVS steps (rad), scalar.
-        M_SS.phstep.v = 0;
+        % ph_step - phase of the PJVS steps (rad), scalar.
+        M_SS.ph_step.v = 0;
         % fm - microwave frequency (Hz), scalar.
         M_SS.fm.v = 75e9;
         % waveformtype - 1: sine, 2: triangular, 3: sawtooth, 4: rectangular.
@@ -100,9 +100,9 @@ function [M_SS] = G_SS(varargin);
         M_SS.f_envelope.v = 1;
         M_SS.A_envelope.v = M_SS.A.v;
         M_SS.ph_envelope.v = 0; %*pi;
-        M_SS.fstep.v = 40*M_SS.f_envelope.v;
+        M_SS.f_step.v = 40*M_SS.f_envelope.v;
         M_SS.L.v = 1.*M_SS.fs.v./M_SS.f_envelope.v;
-        M_SS.phstep.v = 0;
+        M_SS.ph_step.v = 0;
         M_SS.fm.v = 75e9;
         M_SS.waveformtype.v = 2;
         M_SS.Rs.v = M_SS.fs.v./M_SS.f.v;
@@ -115,21 +115,21 @@ function [M_SS] = G_SS(varargin);
                     % % doi: 10.1109/CPEM.2016.7539732:
                     % % f_n = f_J ( n*N + 1 ) => n = (f_n./f_J - 1)./N
                     % % where n must be integer (condition), f_n is frequency of DUT signal, N is number of pjvs steps, f_J is PJVS envelope frequency
-                    % % f = f_envelope * (n * fstep./f_envelope + 1);
-                    % n = (f./f_envelope - 1) ./ (fstep./f_envelope) ;
+                    % % f = f_envelope * (n * f_step./f_envelope + 1);
+                    % n = (f./f_envelope - 1) ./ (f_step./f_envelope) ;
                     % if not(n == fix(n))
                     %     error('bad synchro') %XXX better description
                     % end
 
     % generate PJVS step function:
-    % [y_pjvs, n, Upjvs, Upjvs1period, Spjvs, tsamples] = pjvs_wvfrm_generator2(fs, L, [], f_envelope, A_envelope, ph_envelope, fstep, phstep, fm, waveformtype);
+    % [y_pjvs, n, Upjvs, Upjvs1period, Spjvs, tsamples] = pjvs_wvfrm_generator2(fs, L, [], f_envelope, A_envelope, ph_envelope, f_step, ph_step, fm, waveformtype);
     [y_pjvs, n, M_SS.Upjvs.v, M_SS.Upjvs1period.v, M_SS.Spjvs.v, M_SS.t.v] = pjvs_triangle_generator(M_SS.fs.v, ...
         M_SS.L.v, ...
         [],
         M_SS.f_envelope.v, ...
         M_SS.A_envelope.v, ...
         [], ...
-        M_SS.fstep.v, ...
+        M_SS.f_step.v, ...
         [], ...
         M_SS.fm.v, ...
         [], ...
@@ -143,6 +143,8 @@ function [M_SS] = G_SS(varargin);
     M_SS.y.v = y_dut + y_pjvs;
 
     % Create output structure %<<<1
+    % XXX put somewhere else?
+    M_SS.A_nominal.v = round(M_SS.A.v.*100)./100;
 
     % Verbose info and figure %<<<1
 	if verbose
@@ -155,7 +157,7 @@ function [M_SS] = G_SS(varargin);
         printf('RMS of DUT waveform: %.7f\n', tmp)
         % Signal meant to be used for processing.
         % number of steps per one period of pjvs signal:
-        steps_in_envelope = M_SS.fstep.v./M_SS.f_envelope.v;
+        steps_in_envelope = M_SS.f_step.v./M_SS.f_envelope.v;
         % votlage limit needed to cover whole signal by subsampling:
         limit = 2.*M_SS.A.v./steps_in_envelope; % XXX remove and use value from M_SS
         printf('Usefull +-limit to cover whole waveform: %.7f\n', limit)
@@ -180,8 +182,8 @@ function [M_SS] = G_SS(varargin);
         plot(xl, [limit limit], 'k');
         plot(xl, -1.*[limit limit], 'k');
         % plot step changes
-        for j = 1:numel(Spjvs) - 1
-            plot([tsamples_inv(Spjvs(j)) tsamples_inv(Spjvs(j))], yl, '--b')
+        for j = 1:numel(M_SS.Spjvs.v) - 1
+            plot([tsamples_inv(M_SS.Spjvs.v(j)) tsamples_inv(M_SS.Spjvs.v(j))], yl, '--b')
         end % for j
         legend('PJVS', 'DUT signal', 'PJVS + DUT', 'usefull signal', 'limits for usefull signal')
         title(sprintf('G_SS.m\nf_meas/f_pjvs = %d\nlimit = %.4f', M_SS.f.v./M_SS.f_envelope.v, limit), 'interpreter', 'none')

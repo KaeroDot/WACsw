971645de9361479e47b7f2932ebfb7297066fce8
KaeroDot <KaeroDot@contact.me.on.git.hub>
2025-02-17 08:21:37 +0100

    fix: G_SS now takes input M_SS

--- data_processing/SS/G_SS.m
+++ data_processing/SS/G_SS.m
@@ -1,85 +1,112 @@
 % simulate subsampling measurement data
+% Inputs:
+%   empty
+% OR
+%   M_SS structure
+%   [verbose]
+% OR
+%   predefined_values
+%   [verbose]
 
-function [M_SS] = G_SS(verbose);
+function [M_SS] = G_SS(varargin);
 
-    % inputs XXX 2DO really ?
-
-    % Check inputs %<<<1
-    if ~exist('verbose', 'var')
-        verbose = [];
-    end
-    if isempty(verbose)
+    % Parse and check inputs %<<<1
+    if nargin == 0
+        predefined_values = 1;
+        input1 = 1;
         verbose = 0;
+    elseif nargin == 1
+        input1 = varargin{1};
+        verbose = 0;
+    elseif nargin == 2
+        input1 = varargin{1};
+        verbose = varargin{2};
+    else
+        print_usage();
     end
+
+    if isstruct(input1)
+        M_SS = input1;
+        predefined_values = 0;
+    else
+        if isempty(input1)
+            error('G_SS: predefined values can be only 1, 2 or 3.')
+        end
+        predefined_values = input1(1);
+        if not(any(predefined_values == [1 2 3]))
+            error('G_SS: predefined values can be only 1, 2 or 3.')
+        end
+        M_SS = check_gen_M_SS();
+    end
+
     % ensure verbose is logical:
     verbose = ~(~(verbose));
 
     % Nominal values %<<<1
-    predefined_values = 3;
     if predefined_values == 1
         % These values are working and produce simple plots:
-        f = 100;
-        A = 1;
-        ph = 0;
-        fs = 1e3;
-        f_envelope = 1;
-        A_envelope = A;
-        ph_envelope = 0*pi;
-        fstep = 40*f_envelope;
-        L = 1e3;
-        phstep = 0.0314;
-        fm = 1e14;
-        waveformtype = 2;
-        Rs = 0;
-        Re = Rs;
+        M_SS.f.v = 100;
+        M_SS.A.v = 1;
+        M_SS.ph.v = 0;
+        M_SS.fs.v = 1e3;
+        M_SS.f_envelope.v = 1;
+        M_SS.A_envelope.v = M_SS.A.v;
+        M_SS.ph_envelope.v = 0*pi;
+        M_SS.fstep.v = 40*M_SS.f_envelope.v;
+        M_SS.L.v = 1e3;
+        M_SS.phstep.v = 0.0314;
+        M_SS.fm.v = 1e14;
+        M_SS.waveformtype.v = 2;
+        M_SS.Rs.v = 0;
+        M_SS.Re.v = M_SS.Rs.v;
     elseif predefined_values == 2
         % These values are according Ralf Behr and Luis Palafox 2021 Metrologia 58 025010.
         % DUT signal frequency:
-        f = 100e3;
+        M_SS.f.v = 100e3;
         % DUT signal amplitude:
-        A = 1;
+        M_SS.A.v = 1;
         % DUT signal phase:
-        ph = 0;
+        M_SS.ph.v = 0;
         % sampling frequency (MS/s):
-        fs = 10e6;
+        M_SS.fs.v = 10e6;
         % PJVS envelope signal frequency (Hz):
-        f_envelope = 20;
+        M_SS.f_envelope.v = 20;
         % PJVS envelope Amplitude (V):
-        A_envelope = 1;
+        M_SS.A_envelope.v = 1;
         % PJVS envelope phase (rad):
-        ph_envelope = 0;
+        M_SS.ph_envelope.v = 0;
         % PJVS step change frequency (Hz):
         % (number of PJVS steps in a single envelope (Hz) is given by ratio
         % fstep/f_envelope)
-        fstep = 40.*f_envelope;
+        M_SS.fstep.v = 40.*M_SS.f_envelope.v;
         % Length of the record (samples):
         % one period of the PJVS signal
-        L = fix(1.*fs./f_envelope);
+        M_SS.L.v = fix(1.*M_SS.fs.v./M_SS.f_envelope.v);
         % phstep - phase of the PJVS steps (rad), scalar.
-        phstep = 0;
+        M_SS.phstep.v = 0;
         % fm - microwave frequency (Hz), scalar.
-        fm = 75e9;
+        M_SS.fm.v = 75e9;
         % waveformtype - 1: sine, 2: triangular, 3: sawtooth, 4: rectangular.
-        waveformtype = 2;
+        M_SS.waveformtype.v = 2;
         % number of samples to be removed at start/end of PJVS step:
-        Rs = fs./f; % one period of the signal
-        Re = Rs;
+        M_SS.Rs.v = M_SS.fs.v./M_SS.f.v; % one period of the signal
+        M_SS.Re.v = M_SS.Rs.v;
     elseif predefined_values == 3
         % testing values
-        f = 100;
-        A = 0.9;
-        ph = 0;
-        fs = 100e3;
-        f_envelope = 1;
-        A_envelope = A;
-        ph_envelope = 0; %*pi;
-        fstep = 40*f_envelope;
-        L = 1.*fs./f_envelope;
-        phstep = 0;
-        fm = 75e9;
-        waveformtype = 2;
-        Rs = fs./f;
-        Re = Rs;
+        M_SS.f.v = 100;
+        M_SS.A.v = 0.9;
+        M_SS.ph.v = 0;
+        M_SS.fs.v = 100e3;
+        M_SS.f_envelope.v = 1;
+        M_SS.A_envelope.v = M_SS.A.v;
+        M_SS.ph_envelope.v = 0; %*pi;
+        M_SS.fstep.v = 40*M_SS.f_envelope.v;
+        M_SS.L.v = 1.*M_SS.fs.v./M_SS.f_envelope.v;
+        M_SS.phstep.v = 0;
+        M_SS.fm.v = 75e9;
+        M_SS.waveformtype.v = 2;
+        M_SS.Rs.v = M_SS.fs.v./M_SS.f.v;
+        M_SS.Re.v = M_SS.Rs.v;
     end
 
                     % % calculate condition according Ihlenfeld, ‘Investigations on extending the
@@ -96,30 +123,26 @@ function [M_SS] = G_SS(verbose);
 
     % generate PJVS step function:
     % [y_pjvs, n, Upjvs, Upjvs1period, Spjvs, tsamples] = pjvs_wvfrm_generator2(fs, L, [], f_envelope, A_envelope, ph_envelope, fstep, phstep, fm, waveformtype);
-    [y_pjvs, n, Upjvs, Upjvs1period, Spjvs, tsamples] = pjvs_triangle_generator(fs, L, [], f_envelope, A_envelope, [], fstep, [], fm, [], verbose);
+    [y_pjvs, n, M_SS.Upjvs.v, M_SS.Upjvs1period.v, M_SS.Spjvs.v, M_SS.t.v] = pjvs_triangle_generator(M_SS.fs.v, ...
+        M_SS.L.v, ...
+        [],
+        M_SS.f_envelope.v, ...
+        M_SS.A_envelope.v, ...
+        [], ...
+        M_SS.fstep.v, ...
+        [], ...
+        M_SS.fm.v, ...
+        [], ...
+        verbose);
 
     % DUT signal:
-    y_dut = A.*sin(2.*pi.*f.*tsamples + ph); % add offset XXX: + linspace(0, -1, numel(tsamples));
+    y_dut = M_SS.A.v.*sin(2.*pi.*M_SS.f.v.*M_SS.t.v + M_SS.ph.v); % add offset XXX: + linspace(0, -1, numel(tsamples));
     % starts and ends properly XXX
 
     % digitized signal:
-    y = y_dut + y_pjvs;
+    M_SS.y.v = y_dut + y_pjvs;
 
     % Create output structure %<<<1
-    M_SS = check_gen_M_SS();
-    % Generate measurement data structure:
-    M_SS.fs.v = fs;
-    M_SS.y.v = y;
-    M_SS.t.v = tsamples;
-    M_SS.Upjvs.v = Upjvs;
-    M_SS.Upjvs1period.v = Upjvs1period;
-    M_SS.Spjvs.v = Spjvs;
-    M_SS.f.v = f;
-    M_SS.f_envelope.v = f_envelope;
-    M_SS.f_step.v = fstep;
-    M_SS.A_nominal.v = A;
-    M_SS.Rs.v = Rs;
-    M_SS.Re.v = Re;
 
     % Verbose info and figure %<<<1
 	if verbose
@@ -127,29 +150,29 @@ function [M_SS] = G_SS(verbose);
         disp('---')
         disp('G_SS verbose informations:')
         tmp = sqrt(mean(y_dut.^2)).*sqrt(2);
-        printf('Maximum of DUT + PJVS waveform: %.7f\n', max(y))
-        printf('Minimum of DUT + PJVS waveform: %.7f\n', max(y))
+        printf('Maximum of DUT + PJVS waveform: %.7f\n', max(M_SS.y.v))
+        printf('Minimum of DUT + PJVS waveform: %.7f\n', max(M_SS.y.v))
         printf('RMS of DUT waveform: %.7f\n', tmp)
         % Signal meant to be used for processing.
         % number of steps per one period of pjvs signal:
-        steps_in_envelope = fstep./f_envelope;
+        steps_in_envelope = M_SS.fstep.v./M_SS.f_envelope.v;
         % votlage limit needed to cover whole signal by subsampling:
-        limit = 2.*A./steps_in_envelope;
+        limit = 2.*M_SS.A.v./steps_in_envelope; % XXX remove and use value from M_SS
         printf('Usefull +-limit to cover whole waveform: %.7f\n', limit)
 
         % overview plot
         % set samples above and below limit to NaN:
-        idx = not(and(y < limit, y > -1.*limit));
-        y_usefull = y;
+        idx = not(and(M_SS.y.v < limit, M_SS.y.v > -1.*limit));
+        y_usefull = M_SS.y.v;
         y_usefull(idx) = NaN;
         % time as inverse of period of triangular pjvs waveform:
         % (only for this plot)
-        tsamples_inv = tsamples.*f_envelope;
+        tsamples_inv = M_SS.t.v.*M_SS.f_envelope.v;
 		figure
 		hold on
         plot(tsamples_inv, y_pjvs, 'g-');
         plot(tsamples_inv, y_dut, 'g-.');
-        plot(tsamples_inv, y, '+r');
+        plot(tsamples_inv, M_SS.y.v, '+r');
         plot(tsamples_inv, y_usefull, 'kx');
         xl = xlim();
         yl = ylim();
@@ -161,7 +184,7 @@ function [M_SS] = G_SS(verbose);
             plot([tsamples_inv(Spjvs(j)) tsamples_inv(Spjvs(j))], yl, '--b')
         end % for j
         legend('PJVS', 'DUT signal', 'PJVS + DUT', 'usefull signal', 'limits for usefull signal')
-        title(sprintf('G_SS.m\nf_meas/f_pjvs = %d\nlimit = %.4f', f./f_envelope, limit), 'interpreter', 'none')
+        title(sprintf('G_SS.m\nf_meas/f_pjvs = %d\nlimit = %.4f', M_SS.f.v./M_SS.f_envelope.v, limit), 'interpreter', 'none')
         hold off
         xlabel('time (f_{envelope}^{-1})')
     end % if verbose

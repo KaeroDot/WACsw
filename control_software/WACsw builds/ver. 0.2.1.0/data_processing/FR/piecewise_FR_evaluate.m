0404bd0db76065b98eef1f8d5149430ab2233ccb
KaeroDot <KaeroDot@users.noreply.github.com>
2025-11-28 13:16:47 +0100

    lv: build: ver. 0.2.1.0

---
+++ control_software/WACsw builds/ver. 0.2.1.0/data_processing/FR/piecewise_FR_evaluate.m
@@ -0,0 +1,33 @@
+% XXX proper description
+% Returns errors or what?
+function y_all = piecewise_FR_evaluate(piecewise_fit, f, fs)
+    % Check inputs %<<<1
+    % XXX
+
+    % evaluate piecewise fit piecewise_fit for absolute frequencies f
+    y_all = NaN.*zeros(size(f));
+    f_rel = f./fs.v;
+    if strcmp(piecewise_fit.method, 'spline') %<<<1
+        % Spline method
+        y_all = ppval(piecewise_fit.fit, f_rel);
+    elseif strcmp(piecewise_fit.method, 'polynomial') %<<<1
+        % Polynomial method
+        for j = 1:numel(piecewise_fit.limits) - 1
+            f_low_rel = piecewise_fit.limits(j)./fs.v;
+            f_high_rel = piecewise_fit.limits(j + 1)./fs.v;
+            idx = and(f_rel >= f_low_rel, f_rel <= f_high_rel);
+            % evaluate:
+            tmp = polyval(piecewise_fit.fit.polP{j}, f_rel(idx), piecewise_fit.fit.polS{j}, piecewise_fit.fit.polMU{j});
+            y_all(idx) = tmp;
+        end % for j
+    else
+        error(sprintf('piecewise_FR_fit: unknown method `%s`!', piecewise_fit.method))
+    end % if method
+
+    if any(isnan(y_all))
+        warning('piecewise_FR_evaluate: fit evaluation resulted in NaN values! These values were replaced by 1!')
+        y_all(isnan(y_all)) = 1;
+    end
+end % function piecewise_FR_evaluate
+
+% vim settings modeline: vim: foldmarker=%<<<,%>>> fdm=marker fen ft=matlab
---
+++ control_software/WACsw builds/ver. 0.2.1.0/data_processing/SS/selftest_SS.m
@@ -0,0 +1,38 @@
+clear all, close all
+addpath('../FR')
+addpath('../CE')
+% simulation setup:
+
+verbose = 3;
+% Generate simulated FR measurement and process it:
+% (no verbose because only SS is relevant here)
+[M_FR, simulated_digitizer_FR] = G_FR(0);
+[f, measured_digitizer_FR, ac_source_stability, FR_fit] = P_FR(M_FR, '', 0);
+
+% Generate simulated CE measurement before SS and process it:
+% (no verbose because only SS is relevant here)
+M_CE(1) = G_CE(FR_fit, 0);
+[CE_fit(1)] = P_CE(M_CE(1), 0);
+% Generate simulated subsampling measurement:
+% M_SS = conditions_M_SS(check_gen_M_SS());
+% Use set of prefefined values no. 2:
+M_SS = G_SS(2, verbose);
+% Generate simulated CE measurements after SS and process it:
+M_CE(2) = G_CE(FR_fit, 0);
+[CE_fit(2)] = P_CE(M_CE(2), 0);
+% apply both CE fits (before and after SS) by combining them:
+CE_fit_int = CE_fits_interpolate(CE_fit);
+
+y_filtered = apply_CE_FR_on_samples(M_SS, FR_fit, CE_fit_int, 1, verbose);
+M_SS.y.v = y_filtered;
+
+% Process simulated subsampling measurement:
+[A_rms, A_fft] = P_SS(M_SS, FR_fit, CE_fit_int, verbose);
+disp('SS selftest results:')
+% printf('Nominal amplitude (V): %.7f\n')
+printf('Calculated amplitude from RMS value (V): %.7f\n', A_rms)
+printf('... error (uV): %.3f\n', 1e6.*(M_SS.A_nominal.v - A_rms))
+printf('Calculated amplitude from FFT value (V): %.7f\n', A_fft)
+printf('... error (uV): %.3f\n', 1e6.*(M_SS.A_nominal.v - A_fft))
+
+% vim settings modeline: vim: foldmarker=%<<<,%>>> fdm=marker fen ft=matlab textwidth=80 tabstop=4 shiftwidth=4
